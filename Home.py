d2209c8d5aef9e9f1949210273dab954c7a09a9f
frauke <frauke.albrecht@waterplan.com>
2023-08-09 07:53:50 +0200

    cache uploaded data

--- Home.py
+++ Home.py
@@ -10,7 +10,14 @@ streamlit_analytics.start_tracking()
 
 st.set_page_config(page_title="data app for data science and machine learning")
 
-DATA_URL = 'data/water_potability.csv'
+@st.cache_data(ttl=24*3600)
+def load_data(uploaded_file):
+    DATA_URL = 'data/water_potability.csv'
+    if uploaded_file is None:
+        df = pd.read_csv(DATA_URL)
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+    return df
 
 ## sidebar
 add_logo("images/datenraum_klein.png", height=50)
@@ -25,15 +32,12 @@ st.markdown("# Analyse Your Data with Datenraum!")
 st.markdown("### Upload your own data, visualize it, make statistical analysis and Machine Learning!")
 
 uploaded_file = st.file_uploader("Choose a file")
+df = load_data(uploaded_file)
 st.markdown('Supported file types: csv')
-if uploaded_file is None:
-    df = pd.read_csv(DATA_URL)
 
 st.markdown('---')
 st.markdown("# Use Provided Data")
 st.markdown("To explore the possibilites, you can also use the data provided by default. The data loaded by default can be used to classify the water quality in 'potable' and 'not potable'. It contains several parameters that characterize water quality and a column that says whether the water is potable or not. The data is downloaded from [kaggle](https://www.kaggle.com/datasets/adityakadiwal/water-potability). You can find a more detailed description there. It describes a binary classification problem.")
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
 st.session_state["dataframe"] = df
 
 # show Dataframe
